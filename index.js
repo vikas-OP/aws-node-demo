145f239c5cbe9004e96947bd015efc8629483b27
vikas-OP <67771093+vikas-OP@users.noreply.github.com>
2020-10-01 12:44:52 +0530

    cool

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("NodeJS server on EC2 instance");
+});
+
+app.listen(PORT, () => console.log("server started"));
