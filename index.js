8fc29efbf0f8586f44a4444ea06ad6f3640edb88
vikas-OP <67771093+vikas-OP@users.noreply.github.com>
2020-10-02 10:20:28 +0530

    text changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("NodeJS server on EC2 instance");
+  res.send("NodeJS server on EC2 instance. Vikas. This is server 1");
 });
 
 app.listen(PORT, () => console.log("server started"));
